fa9fcb409d48e698d1c9885ffbc6a447ef78f1ed
Cecile <cecilems@msn.com>
2020-11-17 13:25:16 +0100

    creation du footer

---
+++ footer.php
@@ -0,0 +1,9 @@
+<footer>
+    <h2>Cécile Maillard-Salin</h2>
+    <p>Communicante / développeuse web bilingue en recherche de stage du 22 mars au 18 mai 2021</p>
+</footer>
+
+<script src="js/jquery-3.5.1.min.js"></script>
+<script src="js/script.js"></script>
+</body>
+</html>
