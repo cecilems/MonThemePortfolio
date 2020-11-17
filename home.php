53dfd5434eff62045bdcf99fa8132bebbb84df74
Cecile <cecilems@msn.com>
2020-11-17 13:41:43 +0100

    creation home, search et side

---
+++ home.php
@@ -0,0 +1,312 @@
+<?php
+get_header();
+?>
+
+
+<main> 
+
+<div class="lead">
+    <img class='ronde' src="img/CecilemsprofilCarre.jpg" alt="photo Cecilemsprofil">
+    <div class="guillemet">
+        <img class="guillemetg" src="img/quote-right-solid-jaunep.png" alt="guillemet">
+        <p>
+            Communicante diplômée d'école de commerce, traductrice au parcours axé sur l'international, je me forme désormais à la conception de sites web pour m'ouvrir à de nouvelles perspectives orientées Projet numérique.
+        </p>
+        <img class="guillemetd" src="img/quote-right-solid-jaunep.png" alt="guillemet">
+    </div>
+    <img class="signature" src="img/Signature CMS jaune.png" alt="signature jaune">
+    <h3>Chargée de projet web, bilingue anglais</h3>
+    <a class="btn" href="/img/CV_CecileMS_2020.pdf">Télécharger mon CV</a>
+</div>
+
+<section class="qualites">
+    <p id="autonomie" class="qualite">Autonomie</p>
+    <p id="creative" class="qualite">Créativité</p>
+    <p id="federer" class="qualite">Capacité à fédérer</p>
+    <p id="adaptable" class="qualite">Capacité d'adaptation</p>
+    <p id="proposition" class="qualite">Force de proposition </p>
+</section>
+
+
+<h1 id="idcompetences">Compétences</h1>
+<section class="competence">
+
+        <section class="developpement">  
+            <h2>Développement</h2>   
+            <section class="devarticle">
+            <article>
+                <h3>Langages</h3>
+                    <p>HTML5 / CSS3, <br/> PHP5,<br/> JavaScript,<br/> SQL, XML, UML</p>
+            </article>
+            <article>
+                <h3>Frameworks</h3>
+                    <p>Bootstrap, jQuery, AngularJs, PhpUnit, Ionic, Apache / Cordova</p>
+            </article>
+            <article>
+                <h3>Système</h3>
+                    <p>Windows<br> OS/IOS<br> Android<br> Linux</p>
+                </article>
+            <article>
+                <h3>CMS</h3>
+                    <p>WordPress</p> 
+            </article>
+            <article>
+                <h3>Accessibilité</h3>
+                    <p>Initiative W3C</p>
+            </article>
+            <article>
+                <h3>Web services</h3>
+                    <p>Consommer un web services<br/> Json / XM</p>
+            </article>
+            <article>
+                <h3>Conception</h3>
+                    <p>Indesign<br/> Adobe XD<br/> Figma</p>
+            </article>
+            <article>
+                <h3>Bureautique</h3>
+                    <p>Word +++ <br/> Excel +++<br/>Powerpoint +++ OpenOffice</p>   
+            </article>
+            </section>
+        </section> 
+        <section class="metier">
+            <h2>Compétences métier</h2>
+                <p>Gestion de projets numériques</p>
+                <p>Développement de sites web applications mobiles</p>
+                <p>Communication digitale</p>
+                <p>Campagne d'emailing</p>
+                <p>Communication externe et interne</p>
+                <p>Rédaction de supports de communication (web et imprimé)</p>
+                <p>Analyse et bilan des actions de communication</p>
+                <p>Organisation d'évènements</p>
+                <p>Sélectionner des prestataires, négociation</p>
+                <p>Traduction de documentation informatique anglais-Français</p>
+            <article>
+                <h2>LANGUES</h2> 
+                    <ul>
+                        <li>Anglais : Courant</li>
+                        <li>Allemand : Intermédiaire</li>
+                        <li>Espagnol : Débutant</li>
+                    </ul>
+                                        
+                <h2>PERMIS B et véhicule personnel</h2>
+            </article>  
+        </section>
+</section>
+
+<h1 id="idportfolio">PORTFOLIO</h1>
+<section class="portfolio">
+
+        <article>
+            <img class='blogs' src="img/Merveille_vermillon.JPG" alt="velo">
+            <p>Voir mon futur site de photo</p>
+        </article>
+        <article>
+            <img class='blogs' src="img/kangaroo.jpg" alt="kangaroo">
+            <p>Voir mon futur blog de voyage en Australie</p>                    
+        </article>
+        <article>
+            <img class='blogs' src="img/PeintureConnexion.JPG" alt="peinture">
+            <p>Voir mon futur site de peinture</p>
+         </article>
+
+</section>
+
+<h1 id="idexperience">Expérience</h1> 
+<section class="experience">
+        <article>
+        <h4>Depuis 2015</h4>      
+        <h2>RELECTRICE, TRADUCTRICE</h2> 
+            <h3>CMS COM</h3>
+                <p>Traductions anglais français et relectures de logiciels et documentations technique</p>
+
+            </article>      
+        <article>
+            <h4>2013 | 2014</h4> 
+            <h2>CHARGÉE DE COM ET ÉVÉNEMENT</h2>
+                <h3>ZURFLUH-FELLER</h3>
+                    <p>Plan de communication, Plan média, Marketing direct : mailing via Sarbacane, Organisation d'événements internationaux</p> 
+
+        </article>
+        <article>
+            <h4>2011 | 2012</h4>
+            <h2>ACHETEUSE PROTOTYPES</h2>
+                <h3>PSA Sochaux</h3>
+                    <p>Consultation de fournisseurs frais généraux et prototypes, négociation. Suivi qualité, quantité et délais et coût. Indicateurs de pilotage </p>
+
+        </article>
+
+        <article>
+            <h4>2011</h4>
+            <h2>RESP. COMMERCE & EXPLOITATION</h2>
+                <h3>ACCOR Centre de conférence</h3>
+                     <p>Organisation d'événements, réunions, conférences, séminaires, réceptions, pilotage des prestataires. Comptabilité, analyse du compte de résultats</p>
+
+        </article>
+        <article>
+            <h4>2010 | 2011</h4> 
+            <h2>ASSISTANTE ÉQUIPE PROJET</h2>
+               <h3>PSA Sochaux</h3>
+                <p>Organisation des réunions, déplacements, Gestion de la base documentaire et des groupes de travail- Créations de présentations, newsletter, article</p> 
+
+        </article>
+        <article>
+            <h4>2008</h4>
+            <h2>ASSISTANTE EXPORT</h2>
+                <h3>CRISTEL</h3>
+                  <p>Suivi commercial des clients internationaux, Organisation du transport terrestre, maritime, aérien, Démarches auprès de la Coface et des douanes </p>
+
+        </article>
+        <article>   
+            <h4>2006 | 2008</h4>  
+            <h2>ACHETEUSE SÉRIE</h2>
+              <h3>PSA Hérimoncourt</h3>
+                 <p>Etude des besoins des différents services (Méthodes, Qualité, Approvisionnements), sourcing de fournisseurs, négociation, Suivi des problèmes qualité</p> 
+
+        </article>
+        <article>
+            <h4>2006</h4> 
+            <h2>RESPONSABLE COM INTERNE</h2>
+                <h3>PSA Hérimoncourt</h3>
+                     <p>Membre du comité de direction, Communication managériale cadres et ouvriers, création supports de communication, Organisation des événements du site</p> 
+
+        </article>
+        <article>
+            <h4>2003 | 2004</h4> 
+            <h2>CHARGÉE DE PROJET RSE</h2>
+                <h3>MICROSOFT Paris</h3>
+                    <p>Conception et déploiement de projets à grande couverture médiatique (Docteur Souris, Emmaüs), Pilotage de trois grandes agences de communication</p>
+
+        </article>
+        <article>
+            <h4>2001 | 2002</h4> 
+            <h2>ASSISTANTE MARKETING</h2>
+                 <h3>OBJECTIF LUNE Montréal</h3>
+                    <p>Création de documentation commerciale et technique, newsletter, site web, charte graphique, packaging, Stratégie de comm & lancement de logiciel</p> 
+
+        </article>
+        <article>
+            <h4>1999 | 2000</h4> 
+            <h2>ASSISTANTE MARKETING</h2>   
+                <h3>OSIRIS Montréal</h3>
+                    <p>Rédaction d’outils d’aide à la vente : brochures, présentations, site internet, publicités, articles pour la presse, Organisation de grands salons</p>
+
+        </article>                         
+</section>
+
+<section>
+    <h1 id="idformation">Formation</h1>
+    <section class = "formation">
+
+        <section class="diplome">
+            <h2>Diplômes</h2>
+            <article>
+                <h3>2020</h3>  
+                    <p>Développeur de site web et solutions mobiles pour objets connectés 
+                Formation de 8 mois à Numérica Montbéliard en vue d'obtenir le 
+                titre professionnel de Développeur web et web mobile</p>
+            </article>
+            <article>
+                <h3>1999</h3>
+                    <p>École de Commerce ISEG Strasbourg [Institut Supérieur Européen de Gestion] Diplôme d’Expertise en Management et Affaires Internationales [niveau II — Maîtrise] Spécialisation marketing</p>
+            </article>
+            <article>
+                <h3>1998</h3>
+                    <p>International Business English Communication [Université du Lancashire, Preston, UK] un mois</p>
+            </article>
+            <article>
+                <h3>1995</h3>
+                    <p>Baccalauréat Scientifique option Mathématiques mention Assez Bien</p>
+            </article>
+        </section>
+        <section class="professionnel">
+            <h2>Formation professionnelle</h2>
+            <article>
+                <h3>2014</h3>  
+                    <p>MOOC L’esprit entrepreneurial et Désir d’entreprendre [CNAM]</p> 
+                    <p>MOOC Gestion de projet [Centrale LILLE]</p>                         
+                    <p>MOOC Creative Problem Solving [University of Minnesota]</p>                 
+            </article>
+
+            <article>
+                <h3>2011</h3>  
+                    <p>Sauvetage Secourisme du Travail, 2 jours, Préconis, Belfort</p>
+                    <p>H0B0 Habilitation électrique, personnel non-électricien Préconis, Belfort</p> 
+                    <p>Formation de guide conférencier Villes d’Art et histoire de Franche-Comté</p> 
+            </article>
+            <article>
+                <h3>2001</h3>
+                    <p>Business English : Oral Communication, Université de Montréal, 3 mois</p>
+            </article>
+            <article>
+                <h3>1999</h3>
+                    <p> Les 4 étapes de la production imprimée, formation sur la chaine graphique</p>
+            </article>
+        </section>
+
+        <section class="centredinteret">
+            <h2>Centre d'intérêt</h2>
+                <section class="interet">
+                <p>Lectures, musiques et cuisines du monde</p>
+                <p>Photographe amateur</p>
+                <p>Peinture abstraite accrylique</p>
+                <p>Pratique de la danse classique et du flamenco</p>
+                </section>
+        </section>
+    </section>
+</section>
+<h1 id="idcontact">Contactez-moi</h1>
+<section class="contact">
+    <div class="think">&lt; Think Global &gt; <br/>&lt; ContAct local &gt;</div>
+    <section class= "contactmail">
+        <svg xmlns="http://www.w3.org/2000/svg" width="59" height="78.667" viewBox="0 0 59 78.667">
+                <path id="mouse-solid" d="M0,54.083A24.583,24.583,0,0,0,24.583,78.667h9.833A24.583,24.583,0,0,0,59,54.083V34.417H0ZM27.042,0H24.583A24.583,24.583,0,0,0,0,24.583V29.5H27.042Zm7.375,0H31.958V29.5H59V24.583A24.583,24.583,0,0,0,34.417,0Z"/>
+        </svg>
+        
+            <ul>
+                <li>Nom</li>
+                <li>Email</li>
+                <li>Votre message</li>
+            </ul>
+    </section>
+    <section class="adresse">
+      <svg xmlns="http://www.w3.org/2000/svg" width="59" height="41.766" viewBox="0 0 59 41.766">
+            <path id="address-card-solid" d="M54.083,32H4.917A4.713,4.713,0,0,0,0,36.475V69.291a4.713,4.713,0,0,0,4.917,4.475H54.083A4.713,4.713,0,0,0,59,69.291V36.475A4.713,4.713,0,0,0,54.083,32ZM18.028,40.95c3.616,0,6.556,2.676,6.556,5.967s-2.94,5.967-6.556,5.967-6.556-2.676-6.556-5.967S14.412,40.95,18.028,40.95ZM29.5,63.026a2.088,2.088,0,0,1-2.294,1.79H8.85a2.088,2.088,0,0,1-2.294-1.79v-1.79c0-2.965,3.083-5.37,6.883-5.37h.512a11.515,11.515,0,0,0,8.153,0h.512c3.8,0,6.883,2.405,6.883,5.37ZM52.444,58.1a.788.788,0,0,1-.819.746H36.875a.788.788,0,0,1-.819-.746V56.612a.788.788,0,0,1,.819-.746h14.75a.788.788,0,0,1,.819.746Zm0-5.967a.788.788,0,0,1-.819.746H36.875a.788.788,0,0,1-.819-.746V50.645a.788.788,0,0,1,.819-.746h14.75a.788.788,0,0,1,.819.746Zm0-5.967a.788.788,0,0,1-.819.746H36.875a.788.788,0,0,1-.819-.746V44.679a.788.788,0,0,1,.819-.746h14.75a.788.788,0,0,1,.819.746Z" transform="translate(0 -32)"/>
+          </svg>
+        <p>40 rue Saint-Georges</p>
+        <p>25200 Montbéliard</p>
+        <a id="mail" href="mailto:cecilems@msn.com">cecilems@msn.com</a>
+        <a id="tel" href="tel:+33675956162">Tel : 06 11 01 84 04</a> 
+    </section>
+
+        <section class="reseau" >
+            <a href="https://fr.linkedin.com/in/cecilemaillardsalin">
+                <svg xmlns="http://www.w3.org/2000/svg" width="40.001" height="40" viewBox="0 0 40.001 40">
+                    <path id="linkedin-in-brands" d="M8.954,40.01H.661V13.3H8.954ZM4.8,9.661a4.826,4.826,0,1,1,4.8-4.848A4.843,4.843,0,0,1,4.8,9.661ZM39.992,40.01H31.717v-13c0-3.1-.063-7.072-4.312-7.072-4.312,0-4.972,3.366-4.972,6.848V40.01H14.149V13.3H22.1v3.643h.116a8.714,8.714,0,0,1,7.847-4.313C38.458,12.634,40,18.161,40,25.34V40.01Z" transform="translate(0 -0.01)" fill="#fefefe"/>
+                </svg>
+            </a>
+            <a href="https://www.instagram.com/cecile.maillardsalin/">
+                <svg xmlns="http://www.w3.org/2000/svg" width="40.009" height="40" viewBox="0 0 40.009 40">
+                    <path id="instagram-brands" d="M19.934,41.57A10.255,10.255,0,1,0,30.189,51.825,10.239,10.239,0,0,0,19.934,41.57Zm0,16.923A6.667,6.667,0,1,1,26.6,51.825a6.68,6.68,0,0,1-6.667,6.667ZM33,41.15a2.392,2.392,0,1,1-2.392-2.392A2.387,2.387,0,0,1,33,41.15Zm6.792,2.428c-.152-3.2-.884-6.043-3.231-8.381s-5.177-3.07-8.381-3.231c-3.3-.187-13.2-.187-16.5,0-3.2.152-6.034.884-8.381,3.222S.226,40.365.066,43.569c-.187,3.3-.187,13.2,0,16.5.152,3.2.884,6.043,3.231,8.381s5.177,3.07,8.381,3.231c3.3.187,13.2.187,16.5,0,3.2-.152,6.043-.884,8.381-3.231s3.07-5.177,3.231-8.381C39.981,56.77,39.981,46.88,39.793,43.578ZM35.527,63.616a6.75,6.75,0,0,1-3.8,3.8c-2.633,1.044-8.881.8-11.791.8s-9.167.232-11.791-.8a6.75,6.75,0,0,1-3.8-3.8c-1.044-2.633-.8-8.881-.8-11.791s-.232-9.167.8-11.791a6.75,6.75,0,0,1,3.8-3.8c2.633-1.044,8.881-.8,11.791-.8s9.167-.232,11.791.8a6.75,6.75,0,0,1,3.8,3.8c1.044,2.633.8,8.881.8,11.791S36.571,60.992,35.527,63.616Z" transform="translate(0.075 -31.825)" fill="#fefefe"/>
+                </svg>    
+            </a>
+            <a href="https://www.facebook.com/cecile.maillardsalin" >   
+                <svg xmlns="http://www.w3.org/2000/svg" width="21.423" height="40" viewBox="0 0 21.423 40">
+                <path id="facebook-f-brands" d="M42.91,22.5l1.111-7.239H37.074v-4.7c0-1.98.97-3.911,4.081-3.911h3.158V.489A38.509,38.509,0,0,0,38.708,0c-5.72,0-9.459,3.467-9.459,9.744v5.517H22.89V22.5h6.359V40h7.826V22.5Z" transform="translate(-22.89)" fill="#fefefe"/>
+            </svg></a>
+          </section>
+          
+          
+          
+</section>
+
+
+</section>
+
+</main>  
+
+
+
+
+<?php 
+get_footer();
+?>
