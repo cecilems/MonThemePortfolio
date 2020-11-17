1c9a8e1c69d7540d79e44fe2d74911a87d43589d
Cecile <cecilems@msn.com>
2020-11-17 15:32:55 +0100

    dépannage intégration
    header/footer/affichage css

--- footer.php
+++ footer.php
@@ -5,5 +5,7 @@
 
 <script src="js/jquery-3.5.1.min.js"></script>
 <script src="js/script.js"></script>
+
+<?php wp_footer(); ?>
 </body>
 </html>
--- header.php
+++ header.php
@@ -4,9 +4,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Cécile Maillard-Salin Developeur web</title>
-
-    <link rel="stylesheet" href="css/normalize.css">
-    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="<?php
+    bloginfo('stylesheet_url'); ?>" type="text/css">
+<?php wp_head(); ?>
 
 
 </head>
